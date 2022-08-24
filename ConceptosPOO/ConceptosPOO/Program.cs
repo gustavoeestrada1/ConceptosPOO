825a1cbc2c6a1e57caf4a593d92242ff50bf97fe
GUSTAVO MAURICIO VASQUEZ ESTRADA <gustavovasquez288029@correo.itm.edu.co>
2022-08-24 13:39:07 -0500

    Finish date class

---
+++ ConceptosPOO/ConceptosPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using ConceptosPOO;
+
+Console.WriteLine("POO CONCEPTS");
+
+try
+{
+    Console.WriteLine(new Date(2022, 8, 24));
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(2015, 6, 12));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
+
+
+
